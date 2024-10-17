37531ecbbad5f82d16085a2deb3977f6efd45c8b
EchoNautilus <emir.prawira@ymail.com>
2024-10-17 11:56:13 +0700

    basics.js done for now

--- Trial 3/basics.js
+++ Trial 3/basics.js
@@ -3,12 +3,23 @@
 // Variables
 let myName = "Julian";             // Modern way of declaring a variable
 var myAge = "20";                  // Older way of declaring a variable
-const isDepressed = "true";      // Constant variable. Can't be reassigned
+const isDepressed = true;      // Constant variable. Can't be reassigned
 
 // Functions
 function henloo()
 {
     console.log("I'm depressed. No, I'm actually " + myName + ". And I am " + myAge + ". No, I'm still " + myName + ".");
+    console.log("Are you depressed?");
+
+    // if-else statements
+    if (isDepressed)
+    {
+        console.log("Don't worry. You're a good boy.");
+    }
+    else if (isDepressed == false)
+    {
+        console.log("You're a bad boy. Depression is for cool kids only.")
+    }
 }
 
 // Calling that function
